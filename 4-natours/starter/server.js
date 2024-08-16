c5f4fc32faf22fd185fc1ba8100f5452f5ccb444
mohammedYasser11 <2100921@eng.asu.edu.eg>
2024-08-16 15:22:03 +0300

    connect to db

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,26 @@
-/* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 const port = 3000;
 app.listen(port, () => {
